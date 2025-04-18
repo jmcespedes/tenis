18fab2ab74e8080d6255a36eb0ddf44fb81b7789
jmcespedes <cespedes.josem@gmail.com>
2025-04-17 23:29:49 -0400

    Agrega Procfile y gunicorn

--- app.py
+++ app.py
@@ -67,4 +67,5 @@ def build_twiml_response(message_text):
     return str(response), 200, {'Content-Type': 'text/xml'}
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
+    port = int(os.environ.get("PORT", 5000))  # Usa el puerto proporcionado por Render
+    app.run(host='0.0.0.0', port=port)
